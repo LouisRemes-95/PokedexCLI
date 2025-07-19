8214fb3cc952ceeeaf8b8d23f8e0a0f28c887169
Louis Remes <louisremes@hotmail.be>
2025-07-19 16:59:33 +0200

    cache initialisation

---
+++ internal/pokecache/PokeCache.go
@@ -0,0 +1,47 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type cache struct {
+	CacheEntries map[string]cacheEntry
+	mu           sync.RWMutex
+}
+
+func NewCache(interval time.Duration) *cache {
+	return &cache{
+		CacheEntries: map[string]cacheEntry{},
+	}
+}
+
+func (c *cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.CacheEntries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *cache) Get(key string) ([]byte, bool) {
+	c.mu.RLock()
+	entry, ok := c.CacheEntries[key]
+	c.mu.RUnlock()
+	if ok {
+		return entry.val, ok
+	}
+	return nil, ok
+}
+
+func (c *cache) reapLoop() {
+	for {
+
+	}
+}
